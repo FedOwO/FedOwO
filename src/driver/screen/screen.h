3efcb33efae3acca100b32d24333b0ee0fb92652
Lucas <lucas.doolaeghe@epitech.eu>
2023-03-14 15:30:19 +0100

    [+] Fix wrong cols size for screen

--- src/driver/screen/screen.h
+++ src/driver/screen/screen.h
@@ -5,7 +5,7 @@
 
     #define VIDEO_ADDRESS (char*) 0xb8000
     #define MAX_ROWS 25
-    #define MAX_COLS 160
+    #define MAX_COLS 80
 
     #define REG_SCREEN_CTRL 0x3d4
     #define REG_SCREEN_DATA 0x3d5
