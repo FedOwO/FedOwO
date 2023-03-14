d90463eb244acfff5bf47a9c8501cf01de60115c
louis chabrerie <carotte317@gmail.com>
2023-03-14 16:54:28 +0100

    [+] cos
    Precise modifications:
    Partial cos function

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -4,4 +4,7 @@ void kernel_main() {
     clear_screen();
     kprint("Loading Kernel of FluffOS..\n");
     kprint("Booting into kernel..\n");
+    char *str = (char *)get_cursor_offset();
+    int_to_ascii((int)(100 * cos((float)1, 500)), str);
+    kprint(str);
 }
--- src/libc/math/math.h
+++ src/libc/math/math.h
@@ -1,2 +1,9 @@
-int factorial(int n);
-int fast_pow(int x, int p);
+#ifndef MATH_H_
+    #define MATH_H_
+
+    int factorial(int n);
+    int fast_pow(int x, int p);
+
+    float cos(float x, int precision);
+
+#endif
